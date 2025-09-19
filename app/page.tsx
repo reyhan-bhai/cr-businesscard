96a558c238c159a122ee09572ef8e78177641986
iancode <reyhan@crescentrating.com>
2025-09-19 09:48:51 +0700

    modify the cardSaved section into component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 "use client";
-import Button from "@/components/button";
-import CustomDropdown from "@/components/CustomDropdown";
-import FormField from "@/components/formField";
+import CardSaveComponent from "@/components/sections/cardSavedComponent";
 import MoreInfoSection from "@/components/sections/moreInfoSection";
 import ParseCardSection from "@/components/sections/parseCardSection";
 import StepperComponent from "@/components/stepperComponent";
-import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
@@ -15,21 +12,6 @@ export default function Home() {
   const [filePreview, setFilePreview] = useState<string | undefined>();
   const [isImageParsed, setIsImageParsed] = useState(false);
   // Parse Card Section End
-
-  // More Info Section
-  // const [fullName, setFullName] = useState("eg. John Doe");
-  // const [meetInput, setMeetInput] = useState("e.g., ATM 2025, Dubai");
-  // const [remark, setRemark] = useState(
-  //   "Follow-up items, halal travel interest, etc,"
-  // );
-  // const [yourMessage, setYourMessage] = useState(
-  //   "Follow-up items, halal travel interest, etc,"
-  // );
-  // const [selectedFollowUp, setSelectedFollowUp] = useState("yes");
-  // const [selectedFollowUpType, setSelectedFollowUpType] = useState("");
-
-  // const followUpOptions = ["CR Rating Information", "Others"];
-  // More Info Section End
 
   const handleNextClick = () => {
     if (isImageParsed) {
@@ -86,41 +68,7 @@ export default function Home() {
         handleNextClick={handleNextClick}
       />
 
-
-      <section
-        className={`card-saved-component more-info-page w-full ${
-          currentStep === 3 ? "" : "hidden"
-        } mt-[24px]`}
-      >
-        <div className="p-8 flex flex-col justify-center items-center bg-white rounded-xl outline outline-1 outline-offset-[-1px] outline-stone-300">
-          <Image
-            className=""
-            src="/images/laptop-minimal-check.png"
-            alt="Description of image"
-            width={100}
-            height={100}
-          />
-          <p className="text-black font-['DM_Sans'] mb-4 text-justify">
-            Congratulations! Your card has been successfully saved. Click the
-            button below to access it.{" "}
-          </p>
-
-          <div className="flex flex-col w-full gap-y-[12px]">
-            <Button
-              title="CRHT Contact Sheets"
-              color="bg-[#007D49]"
-              className="w-full  text-white"
-              onClick={handleNextClick}
-            />
-            <Button
-              title="Upload Another Card"
-              color="bg-white-500"
-              className="w-full text-black border border-gray-500"
-              onClick={() => setCurrentStep(1)}
-            />
-          </div>
-        </div>
-      </section>
+      <CardSaveComponent currentStep={currentStep} />
     </>
   );
 }
---
+++ components/sections/cardSavedComponent.tsx
@@ -0,0 +1,48 @@
+import Button from "../button";
+import Image from "next/image";
+
+interface CardSaveComponentProps {
+  currentStep: number;
+}
+const CardSaveComponent: React.FC<CardSaveComponentProps> = ({ currentStep }) => {
+  return (
+    <>
+      <section
+        className={`card-saved-component more-info-page w-full ${
+          currentStep === 3 ? "" : "hidden"
+        } mt-[24px]`}
+      >
+        <div className="p-8 flex flex-col justify-center items-center bg-white rounded-xl outline outline-1 outline-offset-[-1px] outline-stone-300">
+          <Image
+            className=""
+            src="/images/laptop-minimal-check.png"
+            alt="Description of image"
+            width={100}
+            height={100}
+          />
+          <p className="text-black font-['DM_Sans'] mb-4 text-justify">
+            Congratulations! Your card has been successfully saved. Click the
+            button below to access it.{" "}
+          </p>
+
+          <div className="flex flex-col w-full gap-y-[12px]">
+            <Button
+              title="CRHT Contact Sheets"
+              color="bg-[#007D49]"
+              className="w-full  text-white"
+              // onClick={handleNextClick}
+            />
+            <Button
+              title="Upload Another Card"
+              color="bg-white-500"
+              className="w-full text-black border border-gray-500"
+              // onClick={() => setCurrentStep(1)}
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default CardSaveComponent;
